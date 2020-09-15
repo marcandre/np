c685beae203e460547c68ddeefffcbddf98b27ee
Marc-Andre Lafortune <github@marc-andre.ca>
2020-09-15 18:40:44 -0400

    yarn add bootstrap jquery popper.js
    yarn add --dev parcel-bundler @fontawesome/fontawesome-free

--- app/assets/index.js
+++ app/assets/index.js
@@ -2,6 +2,7 @@ import CodeMirror from 'codemirror/lib/codemirror'
 import 'codemirror/addon/edit/matchbrackets'
 import 'codemirror/addon/edit/closebrackets'
 import 'codemirror/mode/ruby/ruby'
+import 'bootstrap'
 
 import App from './app'
 
